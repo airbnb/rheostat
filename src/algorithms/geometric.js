ac671f34f8bff13579b3fa00e038a2627abc37d2
Josh Perez <josh.perez@airbnb.com>
2015-12-10 16:42:18 -0800

    Adds a geometric algorithm

---
+++ src/algorithms/geometric.js
@@ -0,0 +1,9 @@
+export default {
+  getPosition(x, min, max) {
+    return Math.pow(max / (max - min), 0.5) * Math.pow((x - min) / max, 0.5) * 100;
+  },
+
+  getValue(x, min, max) {
+    return Math.floor(Math.pow(x / 100, 2) * (max - min) + min);
+  },
+};
