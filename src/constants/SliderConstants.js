2f6f8b2609174538ed43d72a3317642cdd355be8
Philip Weiss <philip.weiss@airbnb.com>
2018-06-26 16:59:57 -0700

    alphabetize constants

--- src/constants/SliderConstants.js
+++ src/constants/SliderConstants.js
@@ -1,13 +1,13 @@
 export const KEYS = {
+  DOWN: 40,
   END: 35,
   ESC: 27,
   HOME: 36,
   LEFT: 37,
-  DOWN: 40,
-  UP: 38,
   PAGE_DOWN: 34,
   PAGE_UP: 33,
   RIGHT: 39,
+  UP: 38,
 };
 export const PERCENT_EMPTY = 0;
 export const PERCENT_FULL = 100;
