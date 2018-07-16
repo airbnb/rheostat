b69cfe23657b66e7ec54f55ef3f23261c662b071
Philip Weiss <philip.weiss@airbnb.com>
2018-07-16 15:00:47 -0700

    [fix] address nits

--- scripts/utils/registerCSSInterfaceWithDefaultTheme.js
+++ scripts/utils/registerCSSInterfaceWithDefaultTheme.js
@@ -1,9 +1,5 @@
 /* This file should be removed in the next major version update of Rheostat */
 
-import CSSInterface from 'react-with-styles-interface-css';
+import registerCSSInterfaceWithDefaultTheme from '../../lib/utils/registerCSSInterfaceWithDefaultTheme';
 
-import registerInterfaceWithDefaultTheme from './registerInterfaceWithDefaultTheme';
-
-export default function registerCSSInterfaceWithDefaultTheme() {
-  registerInterfaceWithDefaultTheme(CSSInterface);
-}
+export default registerCSSInterfaceWithDefaultTheme;
--- scripts/utils/registerInterfaceWithDefaultTheme.js
+++ scripts/utils/registerInterfaceWithDefaultTheme.js
@@ -1,9 +1,5 @@
 /* This file should be removed in the next major version update of Rheostat */
 
-import ThemedStyleSheet from 'react-with-styles/lib/ThemedStyleSheet';
-import DefaultTheme from '../../src/themes/DefaultTheme';
+import registerInterfaceWithDefaultTheme from '../../lib/utils/registerInterfaceWithDefaultTheme';
 
-export default function registerInterfaceWithDefaultTheme(reactWithStylesInterface) {
-  ThemedStyleSheet.registerInterface(reactWithStylesInterface);
-  ThemedStyleSheet.registerTheme(DefaultTheme);
-}
+export default registerInterfaceWithDefaultTheme;
