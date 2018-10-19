1ecd17c7799dff3bb1f7c37b6f1a5515471afb2a
Ninos Ego <me@ninosego.de>
2018-10-19 13:16:32 +0200

    Fixed: log10 if value = 0

--- src/algorithms/log10.js
+++ src/algorithms/log10.js
@@ -1,16 +1,17 @@
 export default {
   getPosition(value, min, max) {
-    const minv = Math.log(min);
-    const maxv = Math.log(max);
+    const minv = isFinite(Math.log(min)) ? Math.log(min) : 0;
+    const maxv = isFinite(Math.log(max)) ? Math.log(max) : 0;
 
     const scale = (maxv - minv) / 100;
+    const position = (Math.log(value) - minv) / scale;
 
-    return (Math.log(value) - minv) / scale;
+    return isFinite(position) ? position : 0;
   },
 
   getValue(positionPercent, min, max) {
-    const minv = Math.log(min);
-    const maxv = Math.log(max);
+    const minv = isFinite(Math.log(min)) ? Math.log(min) : 0;
+    const maxv = isFinite(Math.log(max)) ? Math.log(max) : 0;
 
     if (positionPercent === 0) {
       return min;
