f3b7ec4b6cff32735ac3d2f00f5a8e3c069aa2ba
Nora Tarano <nora.tarano@airbnb.com>
2019-09-06 15:28:44 -0700

    [fix] Ensure that the root element used for compilation always exists

--- scripts/renderAllComponents.jsx
+++ scripts/renderAllComponents.jsx
@@ -3,4 +3,10 @@ import ReactDOM from 'react-dom';
 
 import LabeledSlider from '../stories/ExampleSlider';
 
-ReactDOM.render(<LabeledSlider />, document.querySelector('#root'));
+const element = document.getElementById('root');
+if (!element) {
+  // Make sure the #root element is defined
+  document.body.innerHTML = '<div id="root" />';
+}
+
+ReactDOM.render(<LabeledSlider />, element);
